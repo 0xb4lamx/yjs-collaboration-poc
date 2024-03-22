adde41c22ae99ecebfd45e1fc9a4509fc802be55
rifaldhiaw <rifaldhiaw@gmail.com>
2024-03-23 00:05:44 +0700

    Add shape and text creation functionality to LeftPanel component

--- app/src/components/LeftPanel.tsx
+++ app/src/components/LeftPanel.tsx
@@ -7,8 +7,59 @@ import {
 } from "lucide-react";
 import { cn } from "../lib/utils";
 import { Col } from "./base/Col";
+import { useMainStore } from "../lib/mainStore";
+import * as fabric from "fabric";
 
 export const LeftPanel = () => {
+  const canvas = useMainStore((state) => state.canvas);
+
+  const handleAddRect = () => {
+    const rect = new fabric.Rect({
+      left: 0,
+      top: 0,
+      fill: "gray",
+      width: 100,
+      height: 100,
+    });
+    canvas.add(rect);
+    canvas.centerObject(rect);
+  };
+
+  const handleAddCircle = () => {
+    const circle = new fabric.Circle({
+      left: 0,
+      top: 0,
+      fill: "gray",
+      radius: 50,
+    });
+    canvas.add(circle);
+    canvas.centerObject(circle);
+  };
+
+  const handleAddTriangle = () => {
+    const triangle = new fabric.Triangle({
+      left: 0,
+      top: 0,
+      fill: "gray",
+      width: 100,
+      height: 100,
+    });
+    canvas.add(triangle);
+    canvas.centerObject(triangle);
+  };
+
+  const handleAddText = () => {
+    const text = new fabric.Textbox("Text", {
+      left: 0,
+      top: 0,
+      fill: "gray",
+      width: 100,
+      height: 100,
+    });
+    canvas.add(text);
+    canvas.centerObject(text);
+  };
+
   return (
     <Col
       center
@@ -24,10 +75,26 @@ export const LeftPanel = () => {
       {/* divier */}
       <div className="w-8 h-0.5 bg-gray-200" />
 
-      <SquareIcon size="20" className="cursor-pointer hover:text-primary" />
-      <CircleIcon size="20" className="cursor-pointer hover:text-primary" />
-      <TriangleIcon size="20" className="cursor-pointer hover:text-primary" />
-      <TypeIcon size="20" className="cursor-pointer hover:text-primary" />
+      <SquareIcon
+        size="20"
+        className="cursor-pointer hover:text-primary"
+        onClick={handleAddRect}
+      />
+      <CircleIcon
+        size="20"
+        className="cursor-pointer hover:text-primary"
+        onClick={handleAddCircle}
+      />
+      <TriangleIcon
+        size="20"
+        className="cursor-pointer hover:text-primary"
+        onClick={handleAddTriangle}
+      />
+      <TypeIcon
+        size="20"
+        className="cursor-pointer hover:text-primary"
+        onClick={handleAddText}
+      />
     </Col>
   );
 };
