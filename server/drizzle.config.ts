460e211c55ca6c8c63d2cbe6d5bb45cadcd64e23
rifaldhiaw <rifaldhiaw@gmail.com>
2024-03-24 15:27:06 +0700

    chore: centralize db

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
   schema: "./src/lib/schema.ts",
   driver: "better-sqlite",
   dbCredentials: {
-    url: "sqlite.db",
+    url: "../db/sqlite.db",
   },
   verbose: true,
   strict: true,
--- server/src/lib/db.ts
+++ server/src/lib/db.ts
@@ -5,7 +5,7 @@ import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import { sessionTable, userTable } from "./schema";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database("../db/sqlite.db");
 
 export const db = drizzle(sqlite);
 export const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
