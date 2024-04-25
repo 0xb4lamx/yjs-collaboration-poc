9cf0ba173aa8e136f5079f1b5c3ceea7931c09b8
0xb4lamx <medghaith.sellami@gmail.com>
2024-04-25 15:48:44 +0200

    feat: poc ws-server persistence

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,37 @@ import { getCookie } from "hono/cookie";
 import { cors } from "hono/cors";
 import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
+import * as Y from 'yjs'
+// @ts-ignore
+import { setupWSConnection, docs, setPersistence, WSSharedDoc } from "y-websocket/bin/utils";
+import { LeveldbPersistence } from 'y-leveldb'
 
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { lucia } from "./lib/auth";
 import { db } from "./lib/db";
 import { trpcRouter } from "./trpcRouter";
-// @ts-ignore
-import { setupWSConnection } from "y-websocket/bin/utils";
+
+const ldb = new LeveldbPersistence('./statedesign-store')
+setPersistence({
+  provider: ldb,
+  bindState: async (docName: string, ydoc: WSSharedDoc) => {
+    //executed on closeConn(), if persistence enabled,will store state then destroy ydocument
+    //ref to https://github.com/yjs/y-websocket/blob/master/bin/utils.cjs#L168
+    const persistedYdoc = await ldb.getYDoc(docName)
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc))
+    //y-leveldb only stores incremental updates
+    ydoc.on('update', (update: Uint8Array) => {
+      ldb.storeUpdate(docName, update)
+    })
+  },
+  writeState: async (_docName: string, _ydoc: WSSharedDoc) => {
+    //executed on closeConn(), if persistence enabled,will store state in DB then destroy ydocument
+    //ref to https://github.com/yjs/y-websocket/blob/master/bin/utils.cjs#L225
+    console.log('no one in the room anymore, let\'s save it to DB')
+    console.log(_ydoc.toJSON())
+    // save to postgreSQL, which type, binary? (performance wise it makes sense! what about readability by different teams?) 
+  }
+})
 
 import type { Session, User } from "lucia";
 import { parseNameFromPath } from "./lib/utils";
@@ -98,6 +122,7 @@ server.api.get("/ws/*", (c) => {
   const docName = parseNameFromPath(c.req.url);
 
   return server.ws(c, {}, (ws) => {
+    //why we need setTimeout here?
     setTimeout(() => {
       setupWSConnection(ws, c.req, { docName });
     }, 0);
@@ -109,5 +134,17 @@ server.api.use("/*", (c, next) => {
 });
 
 server.api.get("*", serveStatic({ path: "./client/index.html" }));
+
+// log some stats
+setInterval(() => {
+  let conns = 0
+  docs.forEach( (doc: WSSharedDoc) => { conns += doc.conns.size })
+  const stats = {
+    conns,
+    docs: docs.size,
+    websocket: `ws://localhost:${3000}`,
+  }
+  console.log(`${new Date().toISOString()} Stats: ${JSON.stringify(stats)}`)
+}, 10000)
 
 server.listen(3000);
